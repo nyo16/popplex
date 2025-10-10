f7f89ef759be790d8a991d58ea2a14b5e6b01b17
Mylan Connolly <mconnolly@iotrak.com>
2025-10-10 11:59:10 -0400

    Prepare package

--- mix.exs
+++ mix.exs
@@ -1,16 +1,45 @@
 defmodule Popplex.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :popplex,
-      version: "0.1.0",
+      version: @version,
+      name: "Popplex",
+      description: "Elixir wrapper for PDF tools using Poppler",
+      source_url: "https://github.com/mylanconnolly/popplex",
+      homepage_url: "https://github.com/mylanconnolly/popplex",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       compilers: [:elixir_make] ++ Mix.compilers(),
       make_targets: ["all"],
-      make_clean: ["clean"]
+      make_clean: ["clean"],
+      package: package(),
+      docs: docs()
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Popplex",
+      extras: ["README.md", ".github/CONTRIBUTING.md", "CHANGELOG.md"],
+      source_ref: "v#{@version}",
+      groups_for_modules: [
+        "Core API": [Popplex],
+        "Native Interface": [Typster.NIF]
+      ]
+    ]
+  end
+
+  defp package do
+    [
+      name: "popplex",
+      files: ~w(lib c_src .formatter.exs mix.exs CHANGELOG.md README.md LICENSE),
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mylanconnolly/typster"}
     ]
   end
 
@@ -24,7 +53,8 @@ defmodule Popplex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:elixir_make, "~> 0.8", runtime: false}
+      {:elixir_make, "~> 0.8", runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
     ]
   end
 end
